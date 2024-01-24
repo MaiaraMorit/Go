883a515a709322f745e165e6b3dd9e9f89e4049d
MaiaraMorit <maiaramorit@gmail.com>
2024-01-24 18:32:37 -0300

    variaveis do mesmo tipo

--- variaveisMesmoTipo.go
+++ variaveisMesmoTipo.go
@@ -3,13 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Maria"
+
+	var nome, email string = "Maria", "maria@hotmail.com"
 	var idade int = 30
-	var email string = "maria@hotmail.com"
-	var altura float64 = 1.65
-	var peso float64 = 65.0
+	var altura, peso float64 = 1.65, 65.0
 
 	fmt.Print("Meu nome é ", nome, " tenho ", idade, " anos, meu email é ", email, " minha altura é ", altura, " e meu peso é ", peso, "kg.")
 }
 
-//go run variaveis.go
+// go run variaveisMesmoTipo.go
