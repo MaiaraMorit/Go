73abaad5aaba71f7d313d5748b228dd9d5579b0c
MaiaraMorit <maiaramorit@gmail.com>
2024-02-01 22:21:38 -0300

    alterando parâmetro

--- retornandoMaisdeUmValor.go
+++ retornandoMaisdeUmValor.go
@@ -7,9 +7,20 @@ func sum(a int, ) (int, int) {
 	return quadrado, cubo
 }
 
+func sum2(a int, ) (quadrado int, cubo int) {
+	quadrado = a * a
+	cubo = a * a * a
+
+	return
+}
+
 func main() {
 	var a int = 10
+	var b int = 20
+
 	quadrado, cubo := sum(a)
+	quadradoDeB, cuboDeB := sum2(b)
 
 	println("O quadrado e o cubo de", a, "são:", quadrado, cubo)
+	println("O quadrado e o cubo de", b, "são:", quadradoDeB, cuboDeB)
 }
